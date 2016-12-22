3f2ece0f67d3719e97cd0c4f4e6409bb573750f8
Higure <siluvion@gmail.com>
2016-12-22 18:41:59 +0100

    LimPop
    Limite de pop + roles

--- configurateur.js
+++ configurateur.js
@@ -1,10 +1,36 @@
-// Ajoute un spawn dans Memory.mySpawn.NOM_SALLE
+// Ajoute un spawn dans Memory.myRooms.NOM_SALLE
 // myRoom : Nom de la salle du spawn
 // mySpawn : Nom du spawn
 
 var addSpawn = function(myRoom, mySpawn)
 {
-	return Memory.mySpawns.myRoom.push(mySpawn);
+	Memory.myRooms[myRoom].spawn = mySpawn;
+};
+
+// Défini le niveau de tech dans Memory.myRooms.NOM_SALLE
+// myRoom : Nom de la salle du spawn
+// tech : Niveau de tech
+
+var setTech = function(myRoom, tech)
+{
+	Memory.myRooms[myRoom].tech = tech;
+};
+
+// Défini le niveau d'énergie maximum dans Memory.myRooms.NOM_SALLE
+// myRoom : Nom de la salle du spawn
+// energyMax : Energie Max
+// ajout : Si energyMax = 0, on ajoute ajout au total d'énergie max précédent
+
+var setEnergyMax = function(myRoom, energyMax, ajout)
+{
+	if(energyMax!=0)
+	{
+		Memory.myRooms[myRoom].energyMax = energyMax;
+	}
+	else
+	{
+		Memory.myRooms[myRoom].energyMax = Memory.myRooms[myRoom].energyMax + ajout;
+	}
 };
 
 var run = function() 
@@ -12,15 +38,17 @@ var run = function()
 	var mySpawn = null;
 	var myRoom = null;
 	
-	if(Memory.mySpawns==null)
+	if(Memory.myRooms==null)
 	{
 		for(var x in Game.spawns)
 		{
 			myRoom = Game.spawns[x].room.name;
 			mySpawn = x;
-			Memory.mySpawns = {};
-			Memory.mySpawns.myRoom = new Array();
+			Memory.myRooms = {};
+			Memory.myRooms[myRoom] = {};
 			addSpawn(myRoom, mySpawn);
+			setTech(myRoom, 1);
+			setEnergyMax(myRoom, 300, 0);
 		}
 	}
 };
@@ -28,5 +56,7 @@ var run = function()
 module.exports = 
 {
     run: run,
+    setTech: setTech,
+    setEnergyMax: setEnergyMax,
 	addSpawn: addSpawn
 };
--- main.js
+++ main.js
@@ -1,23 +1,88 @@
 //console.log("start");
-if(Memory.mySpawns == undefined)
+if(Memory.myRooms == undefined)
 {
-    Memory.mySpawns = null;
-}
+    Memory.myRooms = null;
+	
+	Memory.roles = ["recolteur", "ameliorateur"];
+	Memory.unitsMax = {};
+	
+	for(var i in Memory.roles)
+	{
+		Memory.unitsMax[Memory.roles[i]] = new Array();
+	}
+	
+	Memory.unitsMax.recolteur[1] = 3;
+	Memory.unitsMax.recolteur[2] = 3;
+	Memory.unitsMax.recolteur[3] = 2;
+	Memory.unitsMax.recolteur[4] = 2;
+	Memory.unitsMax.recolteur[5] = 2;
+	Memory.unitsMax.recolteur[6] = 2;
+	Memory.unitsMax.recolteur[7] = 2;
+	Memory.unitsMax.recolteur[8] = 2;
+	
+	Memory.unitsMax.ameliorateur[1] = 3;
+	Memory.unitsMax.ameliorateur[2] = 3;
+	Memory.unitsMax.ameliorateur[3] = 2;
+	Memory.unitsMax.ameliorateur[4] = 2;
+	Memory.unitsMax.ameliorateur[5] = 2;
+	Memory.unitsMax.ameliorateur[6] = 2;
+	Memory.unitsMax.ameliorateur[7] = 2;
+	Memory.unitsMax.ameliorateur[8] = 2;
+	
+	Memory.roles = {};
+	Memory.roles.recolteur = new Array();
+	Memory.roles.recolteur[1] = [WORK, CARRY, CARRY, MOVE, MOVE]; // MAX : 300
+	Memory.roles.recolteur[2] = [WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE]; // MAX : 550
+	Memory.roles.recolteur[3] = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE]; // MAX : 800
+	Memory.roles.recolteur[4] = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE]; // MAX : 1300
+	Memory.roles.recolteur[5] = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE]; // MAX : 1800
+	Memory.roles.recolteur[6] = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE]; // MAX : 2300
+	Memory.roles.recolteur[7] = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE]; // MAX : 2800
+	Memory.roles.recolteur[8] = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE]; // MAX : 3300
 
+	Memory.roles.ameliorateur = new Array();
+	Memory.roles.ameliorateur[1] = [WORK, CARRY, CARRY, MOVE, MOVE]; // MAX : 300
+	Memory.roles.ameliorateur[2] = [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE]; // MAX : 550
+	Memory.roles.ameliorateur[3] = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE]; // MAX : 800
+	Memory.roles.ameliorateur[4] = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE]; // MAX : 1300
+	Memory.roles.ameliorateur[5] = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]; // MAX : 1800
+	Memory.roles.ameliorateur[6] = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]; // MAX : 2300
+	Memory.roles.ameliorateur[7] = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE]; // MAX : 2800
+	Memory.roles.ameliorateur[8] = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]; // MAX : 3300
+	
+}
 
 var configurateur = require('configurateur');
 
 
 
-module.exports.loop = function () 
+module.exports.loop = function() 
 {
-	if(Memory.mySpawns==null)
+	var pop = {};
+	if(Memory.myRooms==null)
 	{
 		configurateur.run();
 	}
-	//console.log(Game.time);
-    /*for (var i in Game.creeps) 
-    {
-	}*/
-	// console.log(Memory);
+	for(var r in Memory.myRooms)
+	{
+		for (var i in Game.creeps) 
+		{
+			var creep = Game.creeps[i];
+			if(creep.memory.role==role)
+			{
+				pop[creep.memory.role]++;
+			}
+		}
+		for(var i in Memory.roles)
+		{
+			if(pop[i]==undefined)
+			{
+				pop[i] = 0;
+			}
+			if(pop[i]<=Memory.unitsMax[i][Memory.myRooms[r].tech])
+			{
+				console.log("Créer un creep : "+i);
+			}
+		}
+	}
 }
